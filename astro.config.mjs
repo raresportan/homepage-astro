be2767eb5922c6e4bc7b8d5c3d50f815e2c26e0d
Rares Portan <rportan@gmail.com>
2022-08-21 21:06:34 +0300

    Big migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://raresportan.com',
-  integrations: [mdx(), sitemap(), image()]
+  markdown: {
+    syntaxHighlight: 'prism',
+  },
+  integrations: [mdx({
+    rehypePlugins: [remarkReadingTime],
+  }), sitemap(), image()]
 });
