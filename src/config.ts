131134793c43b232cb2d87b0ef47a301d1caae8e
Rares Portan <rportan@gmail.com>
2022-08-21 14:21:44 +0300

    Content migration

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Rares Portan';
+export const SITE_DESCRIPTION = `I'm a web developer & designer living in Timisoara, Romania 🇷🇴. I prefer to code in JavaScript and I love simple and fast software 🚀.`;
